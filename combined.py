415ce08d84ed43ec4673b35dc8a8843565d1f48d
V. Khmelevskiy <absxv@yandex.ru>
2019-12-16 12:00:22 +0700

    Refactor

--- combined.py
+++ combined.py
@@ -2,36 +2,20 @@
 import argparse
 import math
 import json
+from collections import namedtuple
 
 import numpy as np
 import cv2 as cv
 from tesserocr import PyTessBaseAPI, PSM, OEM, RIL, iterate_level
 
 import perspective
-results = []
+from objdetect import detect_objects, CLASSES
 
-############ Add argument parser for command line arguments ############
-parser = argparse.ArgumentParser(description='Use this script to run TensorFlow implementation (https://github.com/argman/EAST) of EAST: An Efficient and Accurate Scene Text Detector (https://arxiv.org/abs/1704.03155v2)')
-parser.add_argument('--input', help='Path to input image or video file. Skip this argument to capture frames from a camera.', required=True)
-parser.add_argument('--model', default='./frozen_east_text_detection.pb',
-                    help='Path to a binary .pb file of model contains trained weights.')
-parser.add_argument('--width', type=int, default=320,
-                    help='Preprocess input image by resizing to a specific width. It should be multiple by 32.')
-parser.add_argument('--height',type=int, default=320,
-                    help='Preprocess input image by resizing to a specific height. It should be multiple by 32.')
-parser.add_argument('--thr',type=float, default=0.5,
-                    help='Confidence threshold.')
-parser.add_argument('--nms',type=float, default=0.4,
-                    help='Non-maximum suppression threshold.')
-parser.add_argument('--starttime',type=str, default=None,
-                    help='Optional start time of the range (Video only).')
-parser.add_argument('--endtime',type=str, default=None,
-                    help='Optional end time of the range (Video only).')
-parser.add_argument('--frame',type=int, default=-1,
-                    help='Specify frame number to read at (Video only).')
-parser.add_argument('--info', action='store_true', help='Show video file stats. Skips processing.')
-parser.add_argument('--debug', action='store_true', help='Show window with debug information')
-args = parser.parse_args()
+
+WINDOW_LABEL = 'Debug view'
+
+TextRegion = namedtuple('TextRegion', ['vertices', 'bounding_box'])
+
 
 ############ Utility functions ############
 def decode(scores, geometry, scoreThresh):
@@ -89,6 +73,7 @@ def decode(scores, geometry, scoreThresh):
     # Return detections and confidences
     return [detections, confidences]
 
+
 def get_file_ocr_result(image, rect):
     text = ''
     confidence = 0.0
@@ -104,6 +89,8 @@ def get_blob_ocr_result(image, rect, ppi=0):
     text = ''
     confidence = 0.0
     with PyTessBaseAPI(psm=PSM.SINGLE_LINE) as api:
+        # only read numbers (doesn't seem to work)
+        #api.SetVariable('tessedit_char_whitelist', '0123456789')
         api.SetImageBytes(*image)
         api.SetRectangle(*rect)
         if ppi != 0:
@@ -115,32 +102,172 @@ def get_blob_ocr_result(image, rect, ppi=0):
             confidence = (0,)
     return [text, confidence]
 
+
 def print_video_stats(cap):
     print('FrameRate: ', cap.get(cv.CAP_PROP_FPS))
     print('Dimensions: {} {}'.format(cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
     print('TotalFrames: ', cap.get(cv.CAP_PROP_FRAME_COUNT))
 
+
 def get_wait_time(cap):
-    return int((1/cap.get(cv.CAP_PROP_FPS)) * 1000)
+    """Time in ms to wait before next frame to simulate realtime playback"""
 
-def main():
-    # Read and store arguments
-    confThreshold = args.thr
-    nmsThreshold = args.nms
-    inpWidth = args.width
-    inpHeight = args.height
-    model = args.model
+    if cap.get(cv.CAP_PROP_FRAME_COUNT) < 2:
+        return 1
+    else:
+        return int((1/cap.get(cv.CAP_PROP_FPS)) * 1000)
 
-    # Load network
+
+def ocr_image_region(image, region):
+
+    # -------------------------
+    # deproject image
+    rect = region[0].vertices
+    warped = perspective.four_point_transform(image, rect)
+
+    # -------------------------
+    # get text from image 
+    ocr_image = warped
+    ocr_image = cv.cvtColor(ocr_image, cv.COLOR_RGB2GRAY)
+    img_w = ocr_image.shape[1]
+    img_h = ocr_image.shape[0]
+    tess_rect = (0,0, img_w, img_h)
+    img_bpp = 1
+    img_bpl = int(img_bpp * img_w)
+
+    text, textconf = get_blob_ocr_result((ocr_image.tobytes(), int(img_w), int(img_h), img_bpp, img_bpl), tess_rect)
+
+    # do inverted 2nd pass and pick better result
+    ocr_inv = cv.bitwise_not(ocr_image)
+    text1, textconf1 = get_blob_ocr_result((ocr_inv.tobytes(), int(img_w), int(img_h), img_bpp, img_bpl), tess_rect)
+    if textconf1[0] > textconf[0]:
+        textconf = textconf1
+        text = text1
+
+    return (text, textconf[0])
+
+
+def detect_text_areas(image, model, *args, **kwargs):
+
+    results = []
+
+    # Get frame height and width
+    sourceSize = kwargs['sourceSize']
+    width_ = sourceSize[0]
+    height_ = sourceSize[1]
+    inpWidth = image.shape[1]
+    inpHeight = image.shape[0]
+    rW = width_ / float(inpWidth)
+    rH = height_ / float(inpHeight)
+
+    net = model
+    confThreshold = kwargs['confThreshold']
+    nmsThreshold = kwargs['nmsThreshold']
+    outNames = kwargs['outNames']
+
+    # Create a 4D blob from frame.
+    blob = cv.dnn.blobFromImage(image, 1.0, (inpWidth, inpHeight), (123.68, 116.78, 103.94), True, False)
+
+    # Run the model
+    net.setInput(blob)
+    outs = net.forward(outNames)
+    t, _ = net.getPerfProfile()
+
+    # Get scores and geometry
+    scores = outs[0]
+    geometry = outs[1]
+    [boxes, confidences] = decode(scores, geometry, confThreshold)
+
+    # Apply NMS
+    indices = cv.dnn.NMSBoxesRotated(boxes, confidences, confThreshold,nmsThreshold)
+    for i in indices:
+        # get 4 corners of the rotated rect
+        vertices = cv.boxPoints(boxes[i[0]])
+        # scale the bounding box coordinates based on the respective ratios
+        for j in range(4):
+            vertices[j][0] *= rW
+            vertices[j][1] *= rH
+        for j in range(4):
+            p1 = (vertices[j][0], vertices[j][1])
+            p2 = (vertices[(j + 1) % 4][0], vertices[(j + 1) % 4][1])
+            #cv.line(frame, p1, p2, (0, 255, 0), 1)
+
+        xmin = vertices[0][0]
+        xmax = 0
+        ymin = vertices[0][1]
+        ymax = 0
+        for p in vertices:
+            xmin = int(min(xmin, p[0]))
+            ymin = int(min(ymin, p[1]))
+            xmax = int(max(xmax, p[0]))
+            ymax = int(max(ymax, p[1]))
+
+        results.append((TextRegion(vertices, (xmin, ymin, xmax, ymax)), confidences[i[0]]))
+
+    return results
+
+
+def create_network(model):
     net = cv.dnn.readNet(model)
 
-    # Create a new named window
-    kWinName = "Text OCR Extractor"
-    if args.debug:
-        cv.namedWindow(kWinName, cv.WINDOW_NORMAL)
     outNames = []
     outNames.append("feature_fusion/Conv_7/Sigmoid")
     outNames.append("feature_fusion/concat_3")
+
+    return net, outNames
+
+
+def adjust_dimension(size):
+    """Adjusts size to satisfy EAST requirements"""
+
+    return int(32 * math.ceil(size/32))
+
+
+def set_next_frame(cap, frame):
+    """Rewind capture to specified frame number if possible"""
+
+    if frame != -1 and cap.get(cv.CAP_PROP_FRAME_COUNT) > 1:
+        cap.set(cv.CAP_PROP_POS_FRAMES, frame)
+
+
+def is_too_wide_for(obj, dim):
+    """Filters too wide objects in detected objects list"""
+
+    left, top, right, bottom = obj[2] # rect
+    return (right-left)/dim < 0.5
+
+def main():
+    parser = argparse.ArgumentParser(description='Use this script to run TensorFlow implementation (https://github.com/argman/EAST) of EAST: An Efficient and Accurate Scene Text Detector (https://arxiv.org/abs/1704.03155v2)')
+    parser.add_argument('--input', help='Path to input image or video file.', required=True)
+    parser.add_argument('--model', default='./models/frozen_east_text_detection.pb',
+                        help='Path to a binary .pb file of model contains trained weights.')
+    parser.add_argument('--width', type=int, default=320,
+                        help='Preprocess input image by resizing to a specific width. It should be multiple by 32.')
+    parser.add_argument('--height',type=int, default=320,
+                        help='Preprocess input image by resizing to a specific height. It should be multiple by 32.')
+    parser.add_argument('--thr',type=float, default=0.5,
+                        help='Confidence threshold.')
+    parser.add_argument('--nms',type=float, default=0.4,
+                        help='Non-maximum suppression threshold.')
+    parser.add_argument('--starttime',type=str, default=None,
+                        help='Optional start time of the range (Video only).')
+    parser.add_argument('--endtime',type=str, default=None,
+                        help='Optional end time of the range (Video only).')
+    parser.add_argument('--frame',type=int, default=-1,
+                        help='Specify frame number to read at (Video only).')
+    parser.add_argument('--info', action='store_true', help='Show video file stats. Skips processing.')
+    parser.add_argument('--debug', action='store_true', help='Show window with debug information')
+    args = parser.parse_args()
+
+    # Read and store arguments
+    confThreshold = args.thr
+    nmsThreshold = args.nms
+    inpWidth = adjust_dimension(args.width)
+    inpHeight = adjust_dimension(args.height)
+    model = args.model
+
+    # trained model for object detection
+    objdetectmodel = ('./models/frozen_inference_graph.pb', './models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
 
     # Open a video/image file or a camera stream
     imsrc = args.input
@@ -155,122 +282,54 @@ def main():
         print_video_stats(cap)
         exit()
 
-    if args.frame != -1 and cap.get(cv.CAP_PROP_FRAME_COUNT) > 1:
-        cap.set(cv.CAP_PROP_POS_FRAMES, args.frame)
+    net, outNames = create_network(model)
 
-    # Read initial frame
-    #hasFrame, frame = cap.read()
-    wait_ms = 1 if cap.get(cv.CAP_PROP_FRAME_COUNT) < 2 else get_wait_time(cap)
+    set_next_frame(cap, args.frame)
+    
+    wait_ms = get_wait_time(cap)
+
     while cv.waitKey(wait_ms) < 0:
         # Read frame
         hasFrame, frame = cap.read()
         if not hasFrame:
-            cv.waitKey()
             break
-
-        # make grayscale for later use
-        # pre-filter image
-        thresh_mode = cv.ADAPTIVE_THRESH_GAUSSIAN_C
-        #thresh_mode = cv.ADAPTIVE_THRESH_MEAN_C
-        gray = frame
-        gray = cv.cvtColor(gray, cv.COLOR_RGB2GRAY)
-        #gray = cv.adaptiveThreshold(gray, 255, thresh_mode, cv.THRESH_BINARY, 11, 2)
-        #gray = cv.medianBlur(gray, 3)
-
-        # Get frame height and width
-        height_ = frame.shape[0]
-        width_ = frame.shape[1]
-        rW = width_ / float(inpWidth)
-        rH = height_ / float(inpHeight)
-
-        # Create a 4D blob from frame.
-        blob = cv.dnn.blobFromImage(frame, 1.0, (inpWidth, inpHeight), (123.68, 116.78, 103.94), True, False)
-
-        # Run the model
-        net.setInput(blob)
-        outs = net.forward(outNames)
-        t, _ = net.getPerfProfile()
-        label = 'Inference time: %.2f ms' % (t * 1000.0 / cv.getTickFrequency())
-
-        # Get scores and geometry
-        scores = outs[0]
-        geometry = outs[1]
-        [boxes, confidences] = decode(scores, geometry, confThreshold)
-
-        # Apply NMS
-        indices = cv.dnn.NMSBoxesRotated(boxes, confidences, confThreshold,nmsThreshold)
-        for i in indices:
-            # get 4 corners of the rotated rect
-            vertices = cv.boxPoints(boxes[i[0]])
-            # scale the bounding box coordinates based on the respective ratios
-            for j in range(4):
-                vertices[j][0] *= rW
-                vertices[j][1] *= rH
-            for j in range(4):
-                p1 = (vertices[j][0], vertices[j][1])
-                p2 = (vertices[(j + 1) % 4][0], vertices[(j + 1) % 4][1])
-                cv.line(frame, p1, p2, (0, 255, 0), 1)
-
-            xmin = vertices[0][0]
-            xmax = 0
-            ymin = vertices[0][1]
-            ymax = 0
-            for p in vertices:
-                xmin = int(min(xmin, p[0]))
-                ymin = int(min(ymin, p[1]))
-                xmax = int(max(xmax, p[0]))
-                ymax = int(max(ymax, p[1]))
-            #cv.line(frame, (xmin,ymin), (xmax,ymax), (0, 0, 255), 1)
-            tess_rect = (xmin, ymin, xmax-xmin, ymax-ymin)
-
-            # -------------------------
-            # deproject image
-
-            rect = vertices
-            warped = perspective.four_point_transform(gray, rect)
-
-            # -------------------------
-            # get text from image 
-            ocr_image = warped
-            img_w = ocr_image.shape[1]
-            img_h = ocr_image.shape[0]
-            tess_rect = (0,0, img_w, img_h)
-            img_bpp = 1
-            #img_bpp = int(ocr_image.size / (img_w*img_h))
-            img_bpl = int(img_bpp * img_w)
-
-            text, textconf = get_blob_ocr_result((ocr_image.tobytes(), int(img_w), int(img_h), img_bpp, img_bpl), tess_rect)
-
-            # do inverted 2nd pass and pick better result
-            ocr_inv = cv.bitwise_not(ocr_image)
-            text1, textconf1 = get_blob_ocr_result((ocr_inv.tobytes(), int(img_w), int(img_h), img_bpp, img_bpl), tess_rect)
-            if textconf1[0] > textconf[0]:
-                textconf = textconf1
-                text = text1
-
-            outfmt = '{} [{}%]'.format(text, textconf[0])
-            cv.putText(frame, outfmt, (xmax, ymax), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-
-            # TODO: write results
-            results.append({'rect': {'x': xmin, 'y': ymin, 'x1': xmax, 'y1': ymax}, 'text': text, 'confidence': textconf[0]})
-
-            #print(outfmt)
-            #exit()
-            #cv.imshow(kWinName,ocr_image)
-            #cv.waitKey()
-            # -------------------------
-
-        # Put efficiency information
-        cv.putText(frame, label, (0, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-
-        print(json.dumps(results))
-
-        # Display the frame
-        if args.debug:
-            cv.imshow(kWinName,frame)
-            cv.waitKey(-1)
         
-        cv.destroyAllWindows()
+        # wip stuff
+        #objects = detect_objects(frame, objdetectmodel, threshold=0.25)
+        #filtered_objects = filter(lambda obj: is_too_wide_for(obj, frame.shape[1]), objects)
+
+        # resize source image for text detection
+        resized = cv.resize(frame, (inpWidth, inpHeight))
+
+        text_areas = detect_text_areas(resized, net, sourceSize=(frame.shape[1], frame.shape[0]), outNames=outNames, confThreshold=confThreshold, nmsThreshold=nmsThreshold)
+
+        output = []
+        result = []
+        for region in text_areas:
+            ocr = ocr_image_region(frame, region)
+            result.append(ocr)
+            output.append({
+                'rect': {
+                    'x': region[0].bounding_box[0], 
+                    'y': region[0].bounding_box[1], 
+                    'x1': region[0].bounding_box[2], 
+                    'y1': region[0].bounding_box[3]
+                }, 
+                'text': ocr[0], 
+                'confidence': ocr[1]
+            })
+
+        print(json.dumps(output))
+
+        if args.debug:
+            for i, text in enumerate(text_areas):
+                left, top, right, bottom = text[0].bounding_box
+                cv.rectangle(frame, (int(left), int(top)), (int(right), int(bottom)), (23, 230, 210), thickness=1)
+                cv.putText(frame, '{} [{:.2f}%]'.format(result[i][0], result[i][1]), (int(right), int(bottom)), cv.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0))
+            cv.imshow(WINDOW_LABEL, frame)
+
+    if args.debug:
+        cv.waitKey()
 
 
 if __name__ == "__main__":
